b94ef320cad0a07d28e90fa7a5a612b1cfd1c1c8
dennisvera <dennisvera.z@gmail.com>
2017-04-23 15:25:16 -0400

    Animate Out Zoom Bounce

--- MagicalGrid/ViewController.swift
+++ MagicalGrid/ViewController.swift
@@ -34,6 +34,8 @@ class ViewController: UIViewController {
         view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
     }
     
+    var selectedCell: UIView?
+    
     func handlePan(gesture: UIPanGestureRecognizer) {
         let location = gesture.location(in: view)
         let width = view.frame.width / CGFloat(numViewPerRow)
@@ -42,14 +44,34 @@ class ViewController: UIViewController {
         print(i, j)
         
         let key = "\(i)|\(j)"
-        let cellView = cells[key]
-        cellView?.backgroundColor = .white
+        guard let cellView = cells[key] else {return}
         
-//        for subview in view.subviews {
-//            if subview.frame.contains(location) {
-//                subview.backgroundColor = .black
-//            }
-//        }
+        if selectedCell != cellView {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
+                self.selectedCell?.layer.transform = CATransform3DIdentity
+                
+            }, completion: nil)
+        }
+        
+        selectedCell = cellView
+        
+        view.bringSubview(toFront: cellView)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            
+            cellView.layer.transform = CATransform3DMakeScale(3, 3, 3)
+            
+        }, completion: nil)
+        
+        if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0.25, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                
+                cellView.layer.transform = CATransform3DIdentity
+                
+            }, completion: { (_) in
+            })
+        }
     }
     
     fileprivate func randomColor() -> UIColor {
